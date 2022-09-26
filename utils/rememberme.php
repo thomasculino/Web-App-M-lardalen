96c180184cf7bca171c09930f0ab7f0689209bd0
Franciskid <Franciskid97@gmail.com>
2022-09-26 13:16:05 +0200

    apply

---
+++ utils/rememberme.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+require_once "Auth.php";
+require_once "Util.php";
+
+$auth = new Auth();
+$db_handle = new DBController();
+$util = new Util();
+
+require_once "authCookieSessionValidate.php";
+
+?>
